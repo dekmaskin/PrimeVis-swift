bbf713e1e9242c49ba60ac10b5a77cdb542be88a
Johan K <johankentkarlsson@gmail.com>
2025-03-29 16:02:14 +0100

    Initial Commit

---
+++ PrimeVis/PrimeVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeVisApp.swift
+//  PrimeVis
+//
+//  Created by Johan Karlsson on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct PrimeVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
